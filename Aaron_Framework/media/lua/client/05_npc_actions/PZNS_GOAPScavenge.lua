f22213a3412f09f958bbe4b0254f393273261877
Wiii-1 <markjunemarino@gmail.com>
2025-11-19 19:38:31 +0800

    wrapped actions for GOAP Planner compatibility

--- Aaron_Framework/media/lua/client/05_npc_actions/PZNS_GOAPScavenge.lua
+++ Aaron_Framework/media/lua/client/05_npc_actions/PZNS_GOAPScavenge.lua
@@ -5,9 +5,20 @@ local PZNS_GOAPScavenge = {}
 PZNS_GOAPScavenge.name = "PZNS_GOAP_Scavenge"
 PZNS_GOAPScavenge.preconditions = {isScavengeLocationAvailable = true}
 PZNS_GOAPScavenge.effects = {hasScavengedItem = true}
+<<<<<<< HEAD
 PZNS_GOAPScavenge.cost = 8.0
 
 function PZNS_GOAPScavenge:activate(npcSurvivor, scavengeLocation)
     PZNS_NPCScavenge.execute(npcSurvivor, scavengeLocation)
+=======
+PZNS_GOAPScavenge.cost = 1.0
+
+function PZNS_GOAPScavenge:activate(npcSurvivor, scavengeLocation)
+    if not PZNS_UtilsNPCs.IsNPCSurvivorIsoPlayerValid(npcSurvivor) then
+        return false
+    end
+
+    return PZNS_NPCScavenge.execute(npcSurvivor, scavengeLocation)
+>>>>>>> 6dcdeba (wrapped actions for GOAP Planner compatibility)
 end
 return PZNS_GOAPScavenge;
