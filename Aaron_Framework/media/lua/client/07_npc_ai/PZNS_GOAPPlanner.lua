344c3e44d2d2c8e82c7f672d28a033e2d3049d5b
Wiii-1 <markjunemarino@gmail.com>
2025-11-19 20:12:17 +0800

    planner

--- Aaron_Framework/media/lua/client/07_npc_ai/PZNS_GOAPPlanner.lua
+++ Aaron_Framework/media/lua/client/07_npc_ai/PZNS_GOAPPlanner.lua
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 local actions = {
 	require("05_npc_actions/PZNS_GOAPGunMagazine"),
 	require("05_npc_actions/PZNS_GOAPHuntPlayer"),
@@ -209,3 +210,43 @@ function PZNS_GOAPPlanner.planForNPC(npcSurvivor, goalOrDesired, actionList)
 end
 return PZNS_GOAPPlanner
 
+=======
+local PZNS_UtilsNPCs = require("02_mod_utils/PZNS_UtilsNPCs")
+local PZNS_WorldUtils = require("02_mod_utils/PZNS_WorldUtils")
+
+local PZNS_GOAPPlanner = {}
+
+local actions = {
+    require("05_npc_actions/PZNS_GOAPGunMagazine"),
+    require("05_npc_actions/PZNS_GOAPHuntPlayer"),
+    require("05_npc_actions/PZNS_GOAPPickUpWeapon"),
+    require("05_npc_actions/PZNS_GOAPRunTo"),
+    require("05_npc_actions/PZNS_GOAPScavenge"),
+    require("05_npc_actions/PZNS_GOAPSwitchWeapon"),
+    require("05_npc_actions/PZNS_GOAPWalkTo"),
+    require("05_npc_actions/PZNS_GOAPWeaponAiming"),
+    require("05_npc_actions/PZNS_GOAPWeaponAttack"),
+    require("05_npc_actions/PZNS_GOAPWeaponEquip"),
+    require("05_npc_actions/PZNS_GOAPWeaponReload"),
+}
+
+local function copystate()
+end
+
+local function meetsPreconditions()
+end
+
+local function applyEffects()
+end
+
+local function stateHash()
+end
+
+local function heuristic()
+end
+
+function PZNS_GOAPPlanner.plan(worldState, goal, actions)
+end
+
+return PZNS_GOAPPlanner;
+>>>>>>> 79e9ef7 (planner)
