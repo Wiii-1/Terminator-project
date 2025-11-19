78406824df92cfc28d3afed943b3c95401d0b951
Wiii-1 <markjunemarino@gmail.com>
2025-11-19 20:07:13 +0800

    check

--- Aaron_Framework/media/lua/client/05_npc_actions/PZNS_GOAPSwitchWeapon.lua
+++ Aaron_Framework/media/lua/client/05_npc_actions/PZNS_GOAPSwitchWeapon.lua
@@ -5,10 +5,21 @@ local PZNS_GOAPSwitchWeapon = {}
 PZNS_GOAPSwitchWeapon.name = "PZNS_GOAP_Switch_Weapon"
 PZNS_GOAPSwitchWeapon.preconditions = {isWeaponAvailable = true}
 PZNS_GOAPSwitchWeapon.effects = {hasWeaponEquipped = true}
+<<<<<<< HEAD
 PZNS_GOAPSwitchWeapon.cost = 2.0
 
 function PZNS_GOAPSwitchWeapon:activate(npcSurvivor, weaponItem)
     PZNS_NPCSwitchWeapon.switchNPCWeapon(npcSurvivor, weaponItem)
+=======
+PZNS_GOAPSwitchWeapon.cost = 1.0
+
+function PZNS_GOAPSwitchWeapon:activate(npcSurvivor, weaponItem)
+    if not PZNS_UtilsNPCs.IsNPCSurvivorIsoPlayerValid(npcSurvivor) then
+        return false
+    end
+
+    return PZNS_NPCSwitchWeapon.switchNPCWeapon(npcSurvivor, weaponItem)
+>>>>>>> 3fb9774 (check)
 end
 
 return PZNS_GOAPSwitchWeapon;
