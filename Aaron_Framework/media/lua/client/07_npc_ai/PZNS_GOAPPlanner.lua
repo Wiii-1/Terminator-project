5f4409a14d70d9ab60275d8e800b5d68e6e9abef
Wiii-1 <markjunemarino@gmail.com>
2025-11-19 20:12:17 +0800

    planner

---
+++ Aaron_Framework/media/lua/client/07_npc_ai/PZNS_GOAPPlanner.lua
@@ -0,0 +1,38 @@
+local PZNS_UtilsNPCs = require("02_mod_utils/PZNS_UtilsNPCs")
+local PZNS_WorldUtils = require("02_mod_utils/PZNS_WorldUtils")
+
+local PZNS_GOAPPlanner = {}
+
+local actions = {
+    require("05_npc_actions/PZNS_GOAPGunMagazine"),
+    require("05_npc_actions/PZNS_GOAPHuntPlayer"),
+    require("05_npc_actions/PZNS_GOAPPickUpWeapon"),
+    require("05_npc_actions/PZNS_GOAPRunTo"),
+    require("05_npc_actions/PZNS_GOAPScavenge"),
+    require("05_npc_actions/PZNS_GOAPSwitchWeapon"),
+    require("05_npc_actions/PZNS_GOAPWalkTo"),
+    require("05_npc_actions/PZNS_GOAPWeaponAiming"),
+    require("05_npc_actions/PZNS_GOAPWeaponAttack"),
+    require("05_npc_actions/PZNS_GOAPWeaponEquip"),
+    require("05_npc_actions/PZNS_GOAPWeaponReload"),
+}
+
+local function copystate()
+end
+
+local function meetsPreconditions()
+end
+
+local function applyEffects()
+end
+
+local function stateHash()
+end
+
+local function heuristic()
+end
+
+function PZNS_GOAPPlanner.plan(worldState, goal, actions)
+end
+
+return PZNS_GOAPPlanner;
